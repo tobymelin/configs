e328e67a2624b5ce56a1d9370edc4a5fe817a78a
Toby M <n.tobias.melin@gmail.com>
2023-02-16 10:20:31 +0000

    nvim: Update treesitter configuration

--- nvim/lua/orgmode-conf.lua
+++ nvim/lua/orgmode-conf.lua
@@ -10,7 +10,15 @@ require'nvim-treesitter.configs'.setup {
     enable = true,
     --additional_vim_regex_highlighting = {'org'}, -- Required for spellcheck, some LaTex highlights and code block highlights that do not have ts grammar
   },
-  --ensure_installed = {'org'}, -- Or run :TSUpdate org
+  ensure_installed = {
+    'c',
+    'lua',
+    'help',
+    'markdown_inline',
+    'org',
+    'typescript',
+    'vim',
+  }, -- Or run :TSUpdate org
 }
 
 local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
