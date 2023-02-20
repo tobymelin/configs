2b96d8fe9888c3156b38ead964b02d0a8fc0f1a3
Toby M <n.tobias.melin@gmail.com>
2023-02-20 07:27:38 +0000

    nvim: Fix treesitter config

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,22 +2,26 @@ return {
   {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    opts = {
-      -- If TS highlights are not enabled at all, or disabled via `disable` prop, highlighting will fallback to default Vim syntax highlighting
-      highlight = {
-        enable = true,
-        --additional_vim_regex_highlighting = {'org'}, -- Required for spellcheck, some LaTex highlights and code block highlights that do not have ts grammar
-      },
-      ensure_installed = {
-        'c',
-        'lua',
-        'help',
-        'markdown_inline',
-        'org',
-        'typescript',
-        'vim',
-      }, -- Or run :TSUpdate org
-    }
+    config = function ()
+      require('nvim-treesitter.configs').setup({
+        -- If TS highlights are not enabled at all, or disabled via `disable` prop, highlighting will fallback to default Vim syntax highlighting
+        highlight = {
+          enable = true,
+          --additional_vim_regex_highlighting = {'org'}, -- Required for spellcheck, some LaTex highlights and code block highlights that do not have ts grammar
+        },
+        ensure_installed = {
+          'c',
+          'lua',
+          'help',
+          'markdown',
+          'markdown_inline',
+          'org',
+          'typescript',
+          'vim',
+        },
+        auto_install = true,
+      })
+    end
   },
 }
 
