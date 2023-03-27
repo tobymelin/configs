10338a1df7c0b677043794461d563946de61f808
Toby M <n.tobias.melin@gmail.com>
2023-03-27 17:43:02 +0100

    nvim: Fix <leader>ff clashing mappings

--- nvim/lua/lsp-and-cmp.lua
+++ nvim/lua/lsp-and-cmp.lua
@@ -37,7 +37,7 @@ local on_attach = function(_, bufnr)
   vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
   vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
   vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<space>ff', function() vim.lsp.buf.format { async = true } end, bufopts)
+  vim.keymap.set('n', '<space>lf', function() vim.lsp.buf.format { async = true } end, bufopts)
 end
 
 -- Mappings.
