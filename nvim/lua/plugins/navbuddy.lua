d500d8a002f47bef229339f5626e18ea49e13d80
Toby M <n.tobias.melin@gmail.com>
2023-03-27 22:52:06 +0100

    nvim: Install navbuddy

---
+++ nvim/lua/plugins/navbuddy.lua
@@ -0,0 +1,21 @@
+-- navbuddy (outline view in popup window
+local nmap = require("tm.keymap").nmap
+
+return {
+  {
+    'SmiteshP/nvim-navbuddy',
+    dependencies = {
+      "SmiteshP/nvim-navic",
+      "MunifTanjim/nui.nvim"
+    },
+    config = function ()
+      require('nvim-navbuddy').setup({
+        lsp = {
+          auto_attach = true,
+        },
+      })
+
+      nmap { '<leader>fo', '<cmd>lua require("nvim-navbuddy").open()<cr>' }
+    end
+  },
+}
