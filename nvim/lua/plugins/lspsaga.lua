3dddb1d07700ea60c944561979f57181b066f8ae
Toby M <n.tobias.melin@gmail.com>
2023-04-11 14:01:11 +0100

    nvim: Fix lspsaga config

--- nvim/lua/plugins/lspsaga.lua
+++ nvim/lua/plugins/lspsaga.lua
@@ -2,11 +2,13 @@ return {
   -- LSP UI
   {
     'glepnir/lspsaga.nvim',
+    event = "LspAttach",
+    dependencies = 'neovim/nvim-lspconfig',
     keys = {
       -- vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>")
       { 'K', '<cmd>Lspsaga hover_doc<CR>' },
     },
-    opt = {
+    opts = {
       lightbulb = {
         enable = false
       },
