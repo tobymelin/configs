d5af6c9a990db1c69de7dec1cd9188401d1dbc94
Toby M <n.tobias.melin@gmail.com>
2023-05-17 08:41:24 +0100

    nvim: Enable current line blame by default

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -2,7 +2,7 @@ return {
   'lewis6991/gitsigns.nvim',
   event = 'VeryLazy',
   opts = {
-    current_line_blame = false,
+    current_line_blame = true,
     on_attach = function(bufnr)
       local gs = package.loaded.gitsigns
 
