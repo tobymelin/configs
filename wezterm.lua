a450a3f52a17a39165c6029d6b276d6342e432be
Toby M <n.tobias.melin@gmail.com>
2023-08-26 09:10:19 +0100

    tmux and wezterm additional mappings

--- wezterm.lua
+++ wezterm.lua
@@ -1,5 +1,6 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local w = require('wezterm')
+local wezterm = require('wezterm')
 
 -- This table will hold the configuration.
 local config = {}
@@ -63,8 +64,59 @@ config.colors = {
   },
 }
 
+-- -------------- smart-splits.nvim config start
+-- if you are *NOT* lazy-loading smart-splits.nvim (recommended)
+local function is_vim(pane)
+  -- this is set by the plugin, and unset on ExitPre in Neovim
+  return pane:get_user_vars().IS_NVIM == 'true'
+end
+
+local direction_keys = {
+  Left = 'h',
+  Down = 'j',
+  Up = 'k',
+  Right = 'l',
+  -- reverse lookup
+  h = 'Left',
+  j = 'Down',
+  k = 'Up',
+  l = 'Right',
+}
+
+local function split_nav(resize_or_move, key)
+  return {
+    key = key,
+    mods = resize_or_move == 'resize' and 'META' or 'CTRL',
+    action = w.action_callback(function(win, pane)
+      if is_vim(pane) then
+        -- pass the keys through to vim/nvim
+        win:perform_action({
+          SendKey = { key = key, mods = resize_or_move == 'resize' and 'META' or 'CTRL' },
+        }, pane)
+      else
+        if resize_or_move == 'resize' then
+          win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
+        else
+          win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+        end
+      end
+    end),
+  }
+end
+-- -------------- smart-splits.nvim config end
+
 -- config.leader = { key="b", mods="CTRL" }
 config.keys = {
+  -- move between split panes
+  split_nav('move', 'h'),
+  split_nav('move', 'j'),
+  split_nav('move', 'k'),
+  split_nav('move', 'l'),
+  -- resize panes
+  split_nav('resize', 'h'),
+  split_nav('resize', 'j'),
+  split_nav('resize', 'k'),
+  split_nav('resize', 'l'),
   {
     key = "d",
     mods = "CMD",
@@ -90,26 +142,26 @@ config.keys = {
     mods = "CMD",
     action = wezterm.action.ActivatePaneDirection 'Next',
   },
-  {
-    key = 'H',
-    mods = 'LEADER',
-    action = wezterm.action.AdjustPaneSize { 'Left', 5 },
-  },
-  {
-    key = 'J',
-    mods = 'LEADER',
-    action = wezterm.action.AdjustPaneSize { 'Down', 5 },
-  },
-  {
-    key = 'K',
-    mods = 'LEADER',
-    action = wezterm.action.AdjustPaneSize { 'Up', 5 }
-  },
-  {
-    key = 'L',
-    mods = 'LEADER',
-    action = wezterm.action.AdjustPaneSize { 'Right', 5 },
-  },
+  -- {
+  --   key = 'H',
+  --   mods = 'LEADER',
+  --   action = wezterm.action.AdjustPaneSize { 'Left', 5 },
+  -- },
+  -- {
+  --   key = 'J',
+  --   mods = 'LEADER',
+  --   action = wezterm.action.AdjustPaneSize { 'Down', 5 },
+  -- },
+  -- {
+  --   key = 'K',
+  --   mods = 'LEADER',
+  --   action = wezterm.action.AdjustPaneSize { 'Up', 5 }
+  -- },
+  -- {
+  --   key = 'L',
+  --   mods = 'LEADER',
+  --   action = wezterm.action.AdjustPaneSize { 'Right', 5 },
+  -- },
 }
 
 -- and finally, return the configuration to wezterm
