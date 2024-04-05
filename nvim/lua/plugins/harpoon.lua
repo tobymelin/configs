f81f2a0cfd16ac19799ea3e78c3c52afc36010b3
Toby M. <n.tobias.melin@gmail.com>
2024-04-05 12:38:23 +0100

    nvim: lazy-lock and fixed Harpoon2 settings

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -5,7 +5,11 @@ return {
   lazy = true,
   config = function ()
     local harpoon = require("harpoon")
-    harpoon:setup({})
+    harpoon:setup({
+      settings = {
+        save_on_toggle = true,
+      },
+    })
 
     vim.keymap.set("n", "<leader>hh", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
