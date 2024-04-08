dbb82daa2a87301775653d1b63527e014a743572
Toby M. <n.tobias.melin@gmail.com>
2024-04-08 15:19:57 +0100

    nvim: Fix orgmode config

--- nvim/lua/plugins/orgmode.lua
+++ nvim/lua/plugins/orgmode.lua
@@ -4,19 +4,6 @@ return {
     'nvim-orgmode/orgmode',
     dependencies = 'nvim-treesitter/nvim-treesitter',
     config = function ()
-      -- Load custom tree-sitter grammar for org filetype
-      require('orgmode').setup_ts_grammar()
-
-      local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-      parser_config.org = {
-        install_info = {
-          url = 'https://github.com/milisims/tree-sitter-org',
-          revision = 'main',
-          files = { 'src/parser.c', 'src/scanner.cc' },
-        },
-        filetype = 'org',
-      }
-
       require('orgmode').setup({
         org_agenda_files = {[[~/org/*]], [[~/org/weekly/*]]},
         org_default_notes_file = '~/org/refile.org',
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -6,20 +6,20 @@ return {
     config = function ()
       require('nvim-treesitter.configs').setup({
         -- If TS highlights are not enabled at all, or disabled via `disable` prop, highlighting will fallback to default Vim syntax highlighting
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = {'org'}, -- Required for spellcheck, some LaTex highlights and code block highlights that do not have ts grammar
-        },
+        -- highlight = {
+        --   enable = true,
+        --   additional_vim_regex_highlighting = {'org'}, -- Required for spellcheck, some LaTex highlights and code block highlights that do not have ts grammar
+        -- },
         ensure_installed = {
           'c',
           'lua',
           'help',
           'markdown',
           'markdown_inline',
-          'org',
           'typescript',
           'vim',
         },
+        ignore_install = { 'org' },
         auto_install = true,
         textobjects = {
           select = {
