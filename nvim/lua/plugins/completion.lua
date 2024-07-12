1b599ed21cd449bc0c8238e1e1858df3c365d58d
Toby M. <n.tobias.melin@gmail.com>
2024-07-12 14:02:35 +0100

    nvim: Replace vsnip with luasnip

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -1,7 +1,20 @@
 return {
   'hrsh7th/cmp-nvim-lsp',
-  'hrsh7th/cmp-vsnip',
-  'hrsh7th/vim-vsnip',
+  -- 'hrsh7th/cmp-vsnip',
+  -- 'hrsh7th/vim-vsnip',
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = { "rafamadriz/friendly-snippets" },
+    -- follow latest release.
+    version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+    -- install jsregexp (optional!).
+    build = "make install_jsregexp",
+    lazy = true,
+    config = function()
+      require("luasnip.loaders.from_vscode").lazy_load()
+    end
+  },
+  'saadparwaiz1/cmp_luasnip',
 
   {
     'hrsh7th/nvim-cmp',
@@ -16,8 +29,8 @@ return {
         snippet = {
           -- REQUIRED - you must specify a snippet engine
           expand = function(args)
-            vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-            -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+            -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+            require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
             -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
             -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
           end,
@@ -32,8 +45,8 @@ return {
         sources = cmp.config.sources({
           { name = 'lazydev', group_index = 0 },
           { name = 'nvim_lsp' },
-          { name = 'vsnip' }, -- For vsnip users.
-          -- { name = 'luasnip' }, -- For luasnip users.
+          -- { name = 'vsnip' }, -- For vsnip users.
+          { name = 'luasnip' }, -- For luasnip users.
           -- { name = 'ultisnips' }, -- For ultisnips users.
           -- { name = 'snippy' }, -- For snippy users.
         }, {
